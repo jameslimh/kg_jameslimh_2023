5a87611409dc6319d5f6be26ce41b7a8d392f332
James Lim <haechan.lim@duke.edu>
2021-04-27 04:04:03 -0400

    second commit, now works with multiple digit input

--- main.js
+++ main.js
@@ -5,39 +5,43 @@ var arr = process.argv.slice(2);
 
 var i;
 for(i=0;i<arr.length;i++){
-    switch(arr[i]){
-        case '0':
-            process.stdout.write("Zero");
-            break;
-        case '1':
-            process.stdout.write("One");
-            break;
-        case '2':
-            process.stdout.write("Two");
-            break;
-        case '3':
-            process.stdout.write("Three");
-            break;
-        case '4':
-            process.stdout.write("Four");
-            break;
-        case '5':
-            process.stdout.write("Five");
-            break;
-        case '6':
-            process.stdout.write("Six");
-            break;
-        case '7':
-            process.stdout.write("Seven");
-            break;
-        case '8':
-            process.stdout.write("Eight");
-            break;
-        case '9':
-            process.stdout.write("Nine");
-            break;
-        default:
-            break;
+    var str = arr[i];
+    var j;
+    for(j=0;j<str.length;j++){
+        switch(str.charAt(j)){
+            case '0':
+                process.stdout.write("Zero");
+                break;
+            case '1':
+                process.stdout.write("One");
+                break;
+            case '2':
+                process.stdout.write("Two");
+                break;
+            case '3':
+                process.stdout.write("Three");
+                break;
+            case '4':
+                process.stdout.write("Four");
+                break;
+            case '5':
+                process.stdout.write("Five");
+                break;
+            case '6':
+                process.stdout.write("Six");
+                break;
+            case '7':
+                process.stdout.write("Seven");
+                break;
+            case '8':
+                process.stdout.write("Eight");
+                break;
+            case '9':
+                process.stdout.write("Nine");
+                break;
+            default:
+                break;
+        }
     }
     if(i!=arr.length-1) process.stdout.write(",");
 }
